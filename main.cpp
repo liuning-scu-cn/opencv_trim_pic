d2174473e4dcbcd85dc032cdb9930d0fea417500
liuning <2742229056@qq.com>
2017-11-20 16:24:04 +0800

    add readme

--- main.cpp
+++ main.cpp
@@ -15,28 +15,30 @@ bool is_selecting = false;
 bool flag1 = true;
 bool flag2 = true;
 
-string s_input = "sample/";		//源图像所在文件路径
-string s_output = "result/";		//输出图像文件路径
-string s_format = ".jpg";		//图像格式
+string s_input = "sample/";	//源图像所在文件路径
+string s_output = "result/";	//输出图像文件路径
+string s_format = ".jpg";	//图像格式
+int n = 52;			//图片最大编号
+int m = 1;			//图片最小编号
 
-ostringstream oss_input;		//拼接源图像路径
-ostringstream oss_output;		//拼接输出图像路径
+ostringstream oss_input;	//拼接源图像路径
+ostringstream oss_output;	//拼接输出图像路径
 
 //鼠标监听事件
 void onMouseCallback(int mouseEvent , int x , int y , int flags , void* params)
 {
 	switch (mouseEvent)
 	{
-		case CV_EVENT_LBUTTONDOWN:
+		case CV_EVENT_LBUTTONDOWN:	//鼠标左键按下
 			pt1 = Point(x , y);
 			pt2 = Point(x , y);
 			is_selecting = true;
 			break;
-		case CV_EVENT_MOUSEMOVE:
+		case CV_EVENT_MOUSEMOVE:	//鼠标移动
 			if (is_selecting)
 				pt2 = Point(x , y);
 			break;
-		case CV_EVENT_LBUTTONUP:
+		case CV_EVENT_LBUTTONUP:	//鼠标左键松开
 			pt2 = Point(x , y);
 			is_selecting = false;
 			break;
@@ -54,7 +56,7 @@ void jointString(int i)
 	oss_output << s_output << i << s_format;
 }
 
-//处理不合法的点
+//处理越界的点
 void processPoint(Point &p1 , Point &p2 , Mat image)
 {
 	if (pt1.x < 0)
@@ -80,17 +82,15 @@ void processPoint(Point &p1 , Point &p2 , Mat image)
 
 int main(void)
 {
-	int n = 52;	//图片最大编号
-	int m = 1;	//图片最小编号
 	int i = m;	//当前图片编号
 
 	jointString(i);	//生成源图像和输出图像路径
 
 	Mat image = imread(oss_input.str());	//导入图像
-	Mat show_image;							//备份图像
+	Mat show_image;				//备份图像
 	Mat saveImage(32 , 32 , image.type());	//目标图像,大小32*32px
 
-	namedWindow("image");							//设置窗口名称
+	namedWindow("image");				//设置窗口名称
 	setMouseCallback("image" , onMouseCallback);	//为窗口设置监听事件
 
 	while (flag1)
